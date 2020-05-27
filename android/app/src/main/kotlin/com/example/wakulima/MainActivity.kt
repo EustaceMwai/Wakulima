5e5de646100d6dcb9e00023e455336c142196d19
Eustace Mwai <emwai@onfonmedia.com>
2020-05-27 19:44:12 +0300

    Wakulima first commit

---
+++ android/app/src/main/kotlin/com/example/wakulima/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.wakulima
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Wakulima',
+      theme: ThemeData(
+        primarySwatch: Colors.lightGreen,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Welcome to Wakulima App'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Container(
+        child: Center(
+          child: Text(
+            'Wakulima Dairy App is coming soon',
+            style: TextStyle(color: Colors.blue),
+          ),
+        ),
+      ),
+    );
+  }
+}
